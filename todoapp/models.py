6fa6973624759f983121c093c798d338d3d4a93f
refadkollathodi <refadk007@gmail.com>
2025-11-23 14:44:43 +0530

    user authentication

--- todoapp/models.py
+++ todoapp/models.py
@@ -6,3 +6,11 @@ class TodoItem(Base):
     id=Column(Integer,primary_key=True,index=True)
     user=Column(String(50),index=True)
     task=Column(String(50),index=True)
+class User(Base):
+    __tablename__='users'
+    id=Column(Integer,primary_key=True,index=True)
+    username=Column(String(50),unique=True,index=True)
+    hashed_password=Column(String(100))
+    disabled=Column(Boolean,default=False)
+
+
