2a04d4a65a259ec0a5cf6b102334cdb484515e53
refadkollathodi <refadk007@gmail.com>
2025-12-15 11:04:03 +0530

    updated db

--- todoapp/database.py
+++ todoapp/database.py
@@ -3,17 +3,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "123")
-DB_HOST = os.getenv("DB_HOST", "host.docker.internal")
-DB_NAME = os.getenv("DB_NAME", "todofastapi")
-DB_PORT = os.getenv("DB_PORT", "3307")
-
-
+DB_USER = os.getenv("DB_USER", "todo_user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "todo_pass")
+DB_HOST = os.getenv("DB_HOST", "mysql")      
+DB_NAME = os.getenv("DB_NAME", "todo_db")
+DB_PORT = os.getenv("DB_PORT", "3306")       
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-
-engine=create_engine(DATABASE_URL)
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base=declarative_base()
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
